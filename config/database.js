5d49612950a6516c6a907e8f0c51643e9bab8ffd
tadeukelly <tadeukelly@gmail.com>
2017-03-24 00:31:57 -0300

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 module.exports = {
         //url : 'mongodb://devmongo:Yp6427gol#09@dogen.mongohq.com:10055/app30849020'
-        url : 'mongodb://heroku_3trd015t:YP6427gol@ds131340.mlab.com:31340/heroku_3trd015t'
+        url : 'mongodb://dev:dev@ds131340.mlab.com:31340/heroku_3trd015t'
         //url : 'mongodb://localhost:27017/mydb' //uncomment for local usage
 
 }
