058fa51f981502710b2ee1c5e355c082d015ca53
tadeukelly <tadeukelly@gmail.com>
2017-04-02 00:31:47 -0300

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -27,8 +27,7 @@ router.get("/org/:name", isLoggedIn, function(req, res){
 // route middleware to make sure a user is logged in
 function isLoggedIn(req, res, next) {        
     // if user is authenticated in the session, carry on 
-    if (req.isAuthenticated()){
-     console.log('aqui');
+    if (req.isAuthenticated()){     
 	    return next();
     }else{
     // if they aren't redirect them to the home page
